10185cfb50e29c4079ecf8b7c66d50afa9a3c724
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-12 23:05:55 +0300

    Scripted auto-commit on change (2021-12-12 23:05:55) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab6/Server.java
+++ src/main/java/ru/bmstu/iu9/lab6/Server.java
@@ -1,4 +1,11 @@
 package ru.bmstu.iu9.lab6;
 
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
 public class Server implements Watcher {
+    @Override
+    public void process(WatchedEvent watchedEvent) {
+
+    }
 }
