86a008186023ae4aabddfbe92cb0a6ab87806cd4
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-13 00:48:20 +0300

    Scripted auto-commit on change (2021-12-13 00:48:20) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab6/AnonApp.java
+++ src/main/java/ru/bmstu/iu9/lab6/AnonApp.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class AnonApp {
     public static void main(String[] args) throws IOException {
-        new Server(Integer.parseInt(args[1]));
+        //new Server(Integer.parseInt(args[1]));
+        new Server(8080);
     }
 }
