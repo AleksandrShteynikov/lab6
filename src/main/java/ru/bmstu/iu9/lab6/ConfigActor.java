c81e7bdf186cfb1535d4a925489d85381ab43c86
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-12 23:54:35 +0300

    Scripted auto-commit on change (2021-12-12 23:54:35) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab6/ConfigActor.java
+++ src/main/java/ru/bmstu/iu9/lab6/ConfigActor.java
@@ -1,6 +1,7 @@
 package ru.bmstu.iu9.lab6;
 
 import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,6 +11,15 @@ public class ConfigActor extends AbstractActor {
 
     @Override
     public Receive createReceive() {
+        return ReceiveBuilder.create()
+                .match(PortRequest.class, req -> {
+                    getSender().tell(getRandom(), self());
+                })
+                .match(PortList.class)
+                .build();
+    }
+
+    private String getRandom() {
         return null;
     }
 }
