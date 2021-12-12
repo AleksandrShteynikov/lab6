7bc7197ee511d5ac889b4c5acee890f0df0e7eb7
AleksandrShteynikov <shtjnk91@gmail.com>
2021-12-12 23:21:15 +0300

    Scripted auto-commit on change (2021-12-12 23:21:15) by gitwatch.sh

--- src/main/java/ru/bmstu/iu9/lab6/Server.java
+++ src/main/java/ru/bmstu/iu9/lab6/Server.java
@@ -15,25 +15,35 @@ import akka.stream.ActorMaterializer;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 
+import java.io.IOException;
 import java.util.concurrent.CompletionStage;
+
+import static akka.http.javadsl.server.Directives.route;
 
 public class Server implements Watcher {
 
     private final static String AKKA_SYSTEM_NAME = "AkkaAnonimizer";
     private final static String HOST_NAME = "localhost";
+    private final static String SERVER_MSG = "Server online at http://" + HOST_NAME + ":";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ActorSystem system = ActorSystem.create(AKKA_SYSTEM_NAME);
         ActorRef config = system.actorOf(Props.create(ConfigActor.class));
         final Http http = Http.get(system);
         final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = createRoute(config).flow(system, materializer)
+        int Port = Integer.parseInt(args[1]);
         final CompletionStage<ServerBinding> binding = http.bindAndHandle(routeFlow,
-                ConnectHttp.toHost(HOST_NAME, args[1]));
+                ConnectHttp.toHost(HOST_NAME, Port), materializer);
+        System.out.println(SERVER_MSG + Port);
+        System.in.read();
+        binding.thenCompose(ServerBinding::unbind).thenAccept(unbound -> system.terminate());
     }
 
     private static Route createRoute(ActorRef config) {
-
+        return route(
+                path()
+        );
     }
 
     @Override
